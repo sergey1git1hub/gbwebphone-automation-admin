eff81a44d6b7ab4b8124922e156ac42846a45b24
vshubkin <Vadim.Shubkin@globalbilgi.com>
2017-04-03 16:56:42 +0300

    xPath changed in Enabled

--- src/test/java/webpages/admin_mode/department_list/Enabled.java
+++ src/test/java/webpages/admin_mode/department_list/Enabled.java
@@ -3,16 +3,14 @@ package webpages.admin_mode.department_list;
 
 import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
-import org.openqa.selenium.By;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.$$;
+import static com.codeborne.selenide.Selenide.*;
 
 public class Enabled {
 
-    private SelenideElement enabledSort = $(By.xpath(".//*[@id='departmentDataForm:departmentDataTable_head']//span[text()='Enabled']"));
-    private SelenideElement enabledIs = $(By.xpath(".//*[@id='departmentDataForm:departmentDataTable_head']//select"));
-    private ElementsCollection enabledCollection = $$(By.xpath(".//*[@id='departmentDataForm:departmentDataTable_data']//td[5]"));
+    private SelenideElement enabledSort = $x(".//*[@id='departmentDataForm:departmentDataTable_head']//span[text()='Enabled']");
+    private SelenideElement enabledIs = $x(".//*[@id='departmentDataForm:departmentDataTable_head']//select");
+    private ElementsCollection enabledCollection = $$x(".//*[@id='departmentDataForm:departmentDataTable_data']//td[5]");
 
     //actions
     public void clickEnabledSort() {

dbcd4cac5b51e07ca4970e0a90d987b435892aac
vshubkin <Vadim.Shubkin@globalbilgi.com>
2017-04-03 17:05:00 +0300

    xPath changed in Name

--- src/test/java/webpages/admin_mode/department_list/Name.java
+++ src/test/java/webpages/admin_mode/department_list/Name.java
@@ -3,16 +3,14 @@ package webpages.admin_mode.department_list;
 
 import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
-import org.openqa.selenium.By;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.$$;
+import static com.codeborne.selenide.Selenide.*;
 
 public class Name {
 
-    private SelenideElement nameSort = $(By.xpath(".//*[@id='departmentDataForm:departmentDataTable_head']//span[text()='Name']"));
-    private SelenideElement nameInput = $(By.xpath(".//*[@id='departmentDataForm:departmentDataTable_head']//span[text()='Name']/following-sibling::input"));
-    private ElementsCollection nameCollection = $$(By.xpath(".//*[@id='departmentDataForm:departmentDataTable_data']//td[2]"));
+    private SelenideElement nameSort = $x(".//*[@id='departmentDataForm:departmentDataTable_head']//span[text()='Name']");
+    private SelenideElement nameInput = $x(".//*[@id='departmentDataForm:departmentDataTable_head']//span[text()='Name']/following-sibling::input");
+    private ElementsCollection nameCollection = $$x(".//*[@id='departmentDataForm:departmentDataTable_data']//td[2]");
 
 
     //actions

348c69ea08552d3288cf89b320af84e2480e698b
vshubkin <Vadim.Shubkin@globalbilgi.com>
2017-04-03 18:10:12 +0300

    some xPath changed

--- src/test/java/webpages/admin_mode/department_list/Tenant.java
+++ src/test/java/webpages/admin_mode/department_list/Tenant.java
@@ -5,13 +5,12 @@ import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 import org.openqa.selenium.By;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.$$;
+import static com.codeborne.selenide.Selenide.*;
 
 public class Tenant {
 
-    private SelenideElement tenantInput = $(By.xpath(".//*[@id='departmentDataForm:departmentDataTable_head']//span[text()='Tenant']/following-sibling::input"));
-    private ElementsCollection tenantCollection = $$(By.xpath(".//*[@id='departmentDataForm:departmentDataTable_data']//td[4]"));
+    private SelenideElement tenantInput = $x(".//*[@id='departmentDataForm:departmentDataTable_head']//span[text()='Tenant']/following-sibling::input");
+    private ElementsCollection tenantCollection = $$x(".//*[@id='departmentDataForm:departmentDataTable_data']//td[4]");
 
     //actions
     public void fillTenantInput(String tenant) {
